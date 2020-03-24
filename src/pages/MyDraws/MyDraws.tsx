47999a8dfa4dbe1a5ff5bdec7ad938625a65190e
Max <max.machnik@gmail.com>
2020-03-24 18:07:44 +0100

    Bugs fixing

--- src/pages/MyDraws/MyDraws.tsx
+++ src/pages/MyDraws/MyDraws.tsx
@@ -1,60 +1,61 @@
-import React, { useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 
 // MUI
-import { Typography, List } from '@material-ui/core';
+import { Typography, List } from "@material-ui/core";
 
 // Components
-import PageWrapper from '../../components/PageWrapper/PageWrapper';
-import DrawRow from '../../components/DrawRow/DrawRow';
+import PageWrapper from "../../components/PageWrapper/PageWrapper";
+import DrawRow from "../../components/DrawRow/DrawRow";
 
 // Interfaces
-import { DrawInterface, StateInterface } from '../../interfaces/interfaces';
+import { DrawInterface, StateInterface } from "../../interfaces/interfaces";
 
 const MyDraws = () => {
-	const [usersDraws, userId] = useSelector((state: StateInterface) => [
-		state.usersDraws,
-		state.userId,
-	]);
-	const dispatch = useDispatch();
+  const [usersDraws, userId] = useSelector((state: StateInterface) => [
+    state.usersDraws,
+    state.userId
+  ]);
+  const dispatch = useDispatch();
 
-	useEffect(() => {
-		dispatch({
-			type: 'WATCH_GET_USER_DRAWS_LIST',
-			payload: { userId: userId },
-		});
-	}, [dispatch, userId, usersDraws]);
-	return (
-		<PageWrapper>
-			<Typography variant="h4" component="h2" align="center">
-				Moje losowania
-			</Typography>
-			<List>
-				{usersDraws &&
-					usersDraws.map((draw: DrawInterface) => {
-                        if(draw.results) {
-                            return (
-                                <DrawRow
-                                    title={draw.title}
-                                    date={draw.date as Date}
-                                    _id={draw._id}
-                                    key={draw._id}
-                                    results={draw.results}
-                                />
-                            );
-                        } else {
-                            return (
-                                <DrawRow
-                                    title={draw.title}
-                                    date={draw.date as Date}
-                                    _id={draw._id}
-                                    key={draw._id}
-                                />
-                            );
-                        }
-					})}
-			</List>
-		</PageWrapper>
-	);
+  useEffect(() => {
+    dispatch({
+      type: "WATCH_GET_USER_DRAWS_LIST",
+      payload: { userId: userId }
+    });
+  }, [dispatch, userId, usersDraws]);
+  return (
+    <PageWrapper>
+      <Typography variant="h4" component="h2" align="center">
+        Moje losowania
+      </Typography>
+      <List>
+        {usersDraws &&
+          usersDraws.map((draw: DrawInterface) => {
+            if (draw.results) {
+              console.log(draw);
+              return (
+                <DrawRow
+                  title={draw.title}
+                  date={draw.date as Date}
+                  _id={draw._id}
+                  key={draw._id}
+                  results={draw.results}
+                />
+              );
+            } else {
+              return (
+                <DrawRow
+                  title={draw.title}
+                  date={draw.date as Date}
+                  _id={draw._id}
+                  key={draw._id}
+                />
+              );
+            }
+          })}
+      </List>
+    </PageWrapper>
+  );
 };
 export default MyDraws;
--- src/store/sagas/sagas.tsx
+++ src/store/sagas/sagas.tsx
@@ -6,9 +6,9 @@ import axios from "axios";
 import { DrawInterface, LoginDataInterface } from "../../interfaces/interfaces";
 
 function* loginUser(action: { type: string; payload: LoginDataInterface }) {
-    const { payload } = action;
-    const graphqlQuery = {
-        query: `
+  const { payload } = action;
+  const graphqlQuery = {
+    query: `
             {
                 login(userInput:
                     {
@@ -17,173 +17,172 @@ function* loginUser(action: { type: string; payload: LoginDataInterface }) {
                     })
                 { token userId username email }
             }`
-    };
+  };
 
-    try {
-        const response = yield axios.post("graphql", graphqlQuery);
-        const loginData = response.data.data.login;
-        const { token, username, email, userId } = loginData;
-        localStorage.setItem("token", token);
-        localStorage.setItem("username", username);
-        localStorage.setItem("email", email);
-        localStorage.setItem("userId", userId);
-        yield put({
-            type: actionTypes.USER_LOGIN,
-            payload: response.data.data.login
-        });
-        axios.defaults.headers.common["Authorization"] = token.toString();
-        yield fetchUserDrawsList();
-    } catch (err) {
-        yield put({
-            type: actionTypes.USER_LOGIN_FAILED,
-            payload: { message: err.response.data.errors[0].message }
-        });
-    }
+  try {
+    const response = yield axios.post("graphql", graphqlQuery);
+    const loginData = response.data.data.login;
+    const { token, username, email, userId } = loginData;
+    localStorage.setItem("token", token);
+    localStorage.setItem("username", username);
+    localStorage.setItem("email", email);
+    localStorage.setItem("userId", userId);
+    yield put({
+      type: actionTypes.USER_LOGIN,
+      payload: response.data.data.login
+    });
+    axios.defaults.headers.common["Authorization"] = token.toString();
+    yield fetchUserDrawsList();
+  } catch (err) {
+    yield put({
+      type: actionTypes.USER_LOGIN_FAILED,
+      payload: { message: err.response.data.errors[0].message }
+    });
+  }
 }
 
 function* watchLoginUser() {
-    yield takeLatest("LOGIN_USER_WATCHER", loginUser);
+  yield takeLatest("LOGIN_USER_WATCHER", loginUser);
 }
 
 function* createDraw(action: { type: string; payload: DrawInterface }) {
-    const { title, date, price, creatorsId, participantsIds } = action.payload;
-    const graphqlQuery = {
-        query: `
+  const { title, date, price, creatorsId, participantsIds } = action.payload;
+  const graphqlQuery = {
+    query: `
 			mutation CreateNewDraw($title: String!, $date: String!, $price: Int!, $creatorsID: ID!, $participantsIDs: [ID!]!) { 
 				createDraw(drawInput:
 					{
 						title: $title, 
 						date: $date, 
 						price: $price, 
-						creatorsID: $creatorsID, 
-						participantsIDs: $participantsIDs
+						creator: $creatorsID, 
+						participants: $participantsIDs
 					}
 				) 
 				{_id}
 			}
 		`,
-        variables: {
-            title: title,
-            date: date,
-            price: price,
-            creatorsID: creatorsId,
-            participantsIDs: participantsIds
-        }
-    };
-    try {
-        const response = yield axios.post("graphql", graphqlQuery);
-        const drawId = response.data.data.createDraw._id;
-        console.log(response.data.data.createDraw._id);
-        yield put({
-            type: actionTypes.CREATE_DRAW,
-            payload: { ...action.payload, _id: drawId }
-        });
-    } catch (error) {
-        console.log(error);
+    variables: {
+      title: title,
+      date: date,
+      price: price,
+      creatorsID: creatorsId,
+      participantsIDs: participantsIds
     }
+  };
+  try {
+    const response = yield axios.post("graphql", graphqlQuery);
+    const drawId = response.data.data.createDraw._id;
+    console.log(response.data.data.createDraw._id);
+    yield put({
+      type: actionTypes.CREATE_DRAW,
+      payload: { ...action.payload, _id: drawId }
+    });
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 function* watchCreateDraw() {
-    yield takeEvery("CREATE_DRAW_WATCHER", createDraw);
+  yield takeEvery("CREATE_DRAW_WATCHER", createDraw);
 }
 
 function* deleteDraw(action: { type: string; payload: { drawId: string } }) {
-    const graphqlQuery = {
-        query: `
+  const graphqlQuery = {
+    query: `
             mutation {
                 deleteDraw(drawId: "${action.payload.drawId}") {success}
             }
         `
-    };
-    try {
-        const response = yield axios.post("graphql", graphqlQuery);
-        if (response.statusText === "OK") {
-            yield put({
-                type: actionTypes.DELETE_DRAW,
-                payload: action.payload
-            });
-        }
-    } catch (err) {
-        console.log(err.response);
+  };
+  try {
+    const response = yield axios.post("graphql", graphqlQuery);
+    if (response.statusText === "OK") {
+      yield put({
+        type: actionTypes.DELETE_DRAW,
+        payload: action.payload
+      });
     }
+  } catch (err) {
+    console.log(err.response);
+  }
 }
 
 function* watchDeleteDraw() {
-    yield takeEvery("DELETE_DRAW_WATCHER", deleteDraw);
+  yield takeEvery("DELETE_DRAW_WATCHER", deleteDraw);
 }
 
 function* logoutUser() {
-    localStorage.clear();
-    yield put({ type: actionTypes.USER_LOGOUT });
+  localStorage.clear();
+  yield put({ type: actionTypes.USER_LOGOUT });
 }
 
 function* watchUserLogout() {
-    yield takeEvery("USER_LOGOUT_WATCHER", logoutUser);
+  yield takeEvery("USER_LOGOUT_WATCHER", logoutUser);
 }
 
 function* autoLoginUser(action: { type: string }) {
-    const username = localStorage.getItem("username");
-    const email = localStorage.getItem("email");
-    const userId = localStorage.getItem("userId");
-    const token = localStorage.getItem("token");
-    if (username && email && userId && token) {
-        axios.defaults.headers.common["Authorization"] = token.toString();
-        yield put({
-            type: actionTypes.USER_LOGIN,
-            payload: {
-                username: username,
-                email: email,
-                userId: userId,
-                token: token
-            }
-        });
-    }
+  const username = localStorage.getItem("username");
+  const email = localStorage.getItem("email");
+  const userId = localStorage.getItem("userId");
+  const token = localStorage.getItem("token");
+  if (username && email && userId && token) {
+    axios.defaults.headers.common["Authorization"] = token.toString();
+    yield put({
+      type: actionTypes.USER_LOGIN,
+      payload: {
+        username: username,
+        email: email,
+        userId: userId,
+        token: token
+      }
+    });
+  }
 }
 
 function* watchAutoLoginUser() {
-    yield takeEvery("USER_AUTOLOGIN_WATCHER", autoLoginUser);
+  yield takeEvery("USER_AUTOLOGIN_WATCHER", autoLoginUser);
 }
 
 function* fetchUserDrawsList() {
-    if (axios.defaults.headers.common["Authorization"]) {
-        const graphqlQuery = {
-            query: `{
+  if (axios.defaults.headers.common["Authorization"]) {
+    const graphqlQuery = {
+      query: `{
                 userDraws {
-                    drawsList{ _id title date price participants {_id username} results {_id username email}}
+                    drawsList{ _id title date price participants {_id username} results {_id username email } creator {_id username email} }
                 }
             }`
-        };
-        try {
-            const response = yield axios.post("graphql", graphqlQuery);
-            const drawsList = response.data.data.userDraws.drawsList.map(
-                (draw: DrawInterface) => {
-                    const formatedDate = new Date(
-                        parseInt(draw.date as string)
-                    );
-                    return { ...draw, date: formatedDate };
-                }
-            );
-            yield put({
-                type: actionTypes.SET_DRAWS_LIST,
-                payload: { drawsList: drawsList }
-            });
-        } catch (err) {
-            console.log(err.response);
+    };
+    try {
+      const response = yield axios.post("graphql", graphqlQuery);
+      console.log(response.data);
+      const drawsList = response.data.data.userDraws.drawsList.map(
+        (draw: DrawInterface) => {
+          const formatedDate = new Date(parseInt(draw.date as string));
+          return { ...draw, date: formatedDate };
         }
+      );
+      yield put({
+        type: actionTypes.SET_DRAWS_LIST,
+        payload: { drawsList: drawsList }
+      });
+    } catch (err) {
+      console.log(err.response);
     }
+  }
 }
 
 function* watchFetchUserDrawsList() {
-    yield takeLatest("FETCH_USER_DRAWS_LIST_WATCHER", fetchUserDrawsList);
+  yield takeLatest("FETCH_USER_DRAWS_LIST_WATCHER", fetchUserDrawsList);
 }
 
 export default function* rootSaga() {
-    yield all([
-        watchLoginUser(),
-        watchCreateDraw(),
-        watchDeleteDraw(),
-        watchUserLogout(),
-        watchAutoLoginUser(),
-        watchFetchUserDrawsList()
-    ]);
+  yield all([
+    watchLoginUser(),
+    watchCreateDraw(),
+    watchDeleteDraw(),
+    watchUserLogout(),
+    watchAutoLoginUser(),
+    watchFetchUserDrawsList()
+  ]);
 }
