2d2d7079c82fb36761e0c9df59663bc220903bc6
Max <max.machnik@gmail.com>
2020-03-30 17:58:35 +0200

    Fetching gift for a draw

--- src/components/WishBox/WishBox.tsx
+++ src/components/WishBox/WishBox.tsx
@@ -1,113 +1,119 @@
-import React from 'react';
-import { useHistory } from 'react-router';
+import React from "react";
+import { useHistory } from "react-router";
 
 // MUI
 import {
-	Card,
-	CardHeader,
-	CardContent,
-	CardActions,
-	List,
-	ListItem,
-	ListItemIcon,
-	ListItemText,
-	Button,
-} from '@material-ui/core';
+  Card,
+  CardHeader,
+  CardContent,
+  CardActions,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  Button
+} from "@material-ui/core";
 
 // Icons
 import {
-	Edit,
-	Delete,
-	Check,
-	Link,
-	MonetizationOn,
-	Description,
-} from '@material-ui/icons';
+  Edit,
+  Delete,
+  Check,
+  Link,
+  MonetizationOn,
+  Description,
+  LockOpen
+} from "@material-ui/icons";
 // Types
-import { Wish } from '../../interfaces/WishTypes';
+import { Wish } from "../../interfaces/WishTypes";
 
 interface WishBoxProps {
-	wish: Wish;
-	view: 'full' | 'simple';
-	deleteWish: (_: React.MouseEvent) => void;
-	reserveWish: (wishId: string) => void;
+  wish: Wish;
+  view: "full" | "simple";
+  deleteWish: (_: React.MouseEvent) => void;
+  reserveWish: (wishId: string) => void;
 }
 
 const WishBox: React.FC<WishBoxProps> = ({
-	wish,
-	view,
-	deleteWish,
-	reserveWish,
+  wish,
+  view,
+  deleteWish,
+  reserveWish
 }) => {
-	const history = useHistory();
-	const handleNavigateToEdit = () => {
-		history.push('/edytuj-zyczenie', { originalData: wish });
-	};
+  const history = useHistory();
+  const handleNavigateToEdit = () => {
+    history.push("/edytuj-zyczenie", { originalData: wish });
+  };
 
-	const handleReserveWish = () => {
-		reserveWish(wish._id);
-	};
-	return (
-		<Card key={wish._id} style={{ margin: '1rem 0', padding: '1rem' }}>
-			<CardHeader title={wish.title} />
-			<CardContent>
-				<List disablePadding>
-					<ListItem>
-						<ListItemIcon>
-							<MonetizationOn />
-						</ListItemIcon>
-						<ListItemText
-							primary={wish.price + ' zł'}
-							secondary="Orientacyjna cena"
-						/>
-					</ListItem>
-					{wish.link && (
-						<ListItem button component="a" href={wish.link}>
-							<ListItemIcon>
-								<Link />
-							</ListItemIcon>
-							<ListItemText primary="Zobacz specyfikację lub zdjęcie" />
-						</ListItem>
-					)}
-					{wish.description && (
-						<ListItem>
-							<ListItemIcon>
-								<Description />
-							</ListItemIcon>
-							<ListItemText>{wish.description}</ListItemText>
-						</ListItem>
-					)}
-				</List>
-			</CardContent>
-			<CardActions>
-				{view === 'full' ? (
-					<>
-						<Button
-							color="secondary"
-							startIcon={<Delete />}
-							onClick={deleteWish}
-						>
-							Usuń
-						</Button>
-						<Button
-							color="primary"
-							startIcon={<Edit />}
-							onClick={handleNavigateToEdit}
-						>
-							Edytuj
-						</Button>
-					</>
-				) : (
-					<Button
-						color="primary"
-						startIcon={<Check />}
-						onClick={handleReserveWish}
-					>
-						Kupię to
-					</Button>
-				)}
-			</CardActions>
-		</Card>
-	);
+  const handleReserveWish = () => {
+    reserveWish(wish._id);
+  };
+
+  const handleCancelReservation = () => {
+    console.log("canceling reservation");
+  };
+  return (
+    <Card key={wish._id} style={{ margin: "1rem 0", padding: "1rem" }}>
+      <CardHeader title={wish.title} />
+      <CardContent>
+        <List disablePadding>
+          <ListItem>
+            <ListItemIcon>
+              <MonetizationOn />
+            </ListItemIcon>
+            <ListItemText
+              primary={wish.price + " zł"}
+              secondary="Orientacyjna cena"
+            />
+          </ListItem>
+          {wish.link && (
+            <ListItem button component="a" href={wish.link}>
+              <ListItemIcon>
+                <Link />
+              </ListItemIcon>
+              <ListItemText primary="Zobacz specyfikację lub zdjęcie" />
+            </ListItem>
+          )}
+          {wish.description && (
+            <ListItem>
+              <ListItemIcon>
+                <Description />
+              </ListItemIcon>
+              <ListItemText>{wish.description}</ListItemText>
+            </ListItem>
+          )}
+        </List>
+      </CardContent>
+      <CardActions>
+        <Button
+          color="primary"
+          startIcon={<LockOpen />}
+          onClick={handleCancelReservation}
+        >
+          Anuluj rezerwację
+        </Button>
+        {view === "full" ? (
+          <>
+            c
+            <Button
+              color="primary"
+              startIcon={<Edit />}
+              onClick={handleNavigateToEdit}
+            >
+              Edytuj
+            </Button>
+          </>
+        ) : (
+          <Button
+            color="primary"
+            startIcon={<Check />}
+            onClick={handleReserveWish}
+          >
+            Kupię to
+          </Button>
+        )}
+      </CardActions>
+    </Card>
+  );
 };
 export default WishBox;
--- src/interfaces/WishTypes.tsx
+++ src/interfaces/WishTypes.tsx
@@ -1,22 +1,23 @@
 import { User } from "./interfaces";
 
 export interface WishInput {
-    title: string;
-    link: string;
-    description: string;
-    price: number | string;
-    _id?: string;
+  title: string;
+  link: string;
+  description: string;
+  price: number | string;
+  _id?: string;
 }
 
 export interface Wish extends WishInput {
-    _id: string;
-    creator: string;
-    price: number;
-    buyer?: string;
+  _id: string;
+  creator: string;
+  price: number;
+  buyer?: string;
+  reserved?: boolean;
 }
 
 export interface WishesListProps {
-    wishesList: Wish[];
-    viewMode: "creator" | "guest";
-    reserveWish?: any;
+  wishesList: Wish[];
+  viewMode: "creator" | "guest";
+  reserveWish?: any;
 }
