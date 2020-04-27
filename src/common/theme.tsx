706142b190b1153be1d6a494761bcb294884809a
Bruinen <max.machnik@gmail.com>
2020-04-27 21:51:08 +0200

    Minor bugs fixed

--- src/common/theme.tsx
+++ src/common/theme.tsx
@@ -1,33 +1,36 @@
-import { createMuiTheme } from "@material-ui/core";
+import { createMuiTheme, responsiveFontSizes } from "@material-ui/core";
 
-export const theme = createMuiTheme({
-  palette: {
-    primary: {
-      main: "#388e3c"
+let theme = createMuiTheme({
+    palette: {
+        primary: {
+            main: "#388e3c",
+        },
+        secondary: {
+            main: "#e53935",
+        },
     },
-    secondary: {
-      main: "#e53935"
-    }
-  },
-  typography: {
-    h1: {
-      fontFamily: "Poiret One"
+    typography: {
+        h1: {
+            fontFamily: "Poiret One",
+        },
+        h2: {
+            fontFamily: "Poiret One",
+        },
+        h3: {
+            fontFamily: "Poiret One",
+        },
+        h4: {
+            fontFamily: "Poiret One",
+            marginBottom: "2rem",
+        },
+        h5: {
+            fontFamily: "Poiret One",
+        },
+        h6: {
+            fontFamily: "Poiret One",
+        },
     },
-    h2: {
-      fontFamily: "Poiret One"
-    },
-    h3: {
-      fontFamily: "Poiret One"
-    },
-    h4: {
-      fontFamily: "Poiret One",
-      marginBottom: "2rem"
-    },
-    h5: {
-      fontFamily: "Poiret One"
-    },
-    h6: {
-      fontFamily: "Poiret One"
-    }
-  }
 });
+
+theme = responsiveFontSizes(theme);
+export { theme };
