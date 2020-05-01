88a0454942222bcd19e20a3e73ad37d2e01a5ae0
bruinen <max.machnik@gmail.com>
2020-05-01 10:40:14 +0200

    Reset password done

--- src/pages/ResetPassword/ResetPassword.tsx
+++ src/pages/ResetPassword/ResetPassword.tsx
@@ -1,65 +1,70 @@
-import React from "react";
-import { useForm } from "react-hook-form";
-import axios from "axios";
+import React from 'react';
+import { useForm } from 'react-hook-form';
+import axios from 'axios';
+import { useHistory } from 'react-router-dom';
 
 // Components
-import PageWrapper from "../../components/PageWrapper/PageWrapper";
+import PageWrapper from '../../components/PageWrapper/PageWrapper';
 
 // MUI
-import { Typography, TextField, Button } from "@material-ui/core";
+import { Typography, TextField, Button } from '@material-ui/core';
 
 export const ResetPassword: React.FC = () => {
-    const { handleSubmit, register, errors } = useForm();
+	const history = useHistory();
+	const { handleSubmit, register, errors } = useForm();
 
-    const onSubmit = async (formData: any) => {
-        const { email } = formData;
-        console.log("SENDING EMAIL TO: ", email);
-        const graphQLquery = {
-            query: `
+	const onSubmit = async (formData: any) => {
+		const { email } = formData;
+		console.log('SENDING EMAIL TO: ', email);
+		const graphQLquery = {
+			query: `
             mutation{sendResetPasswordEmail(email: "${email}") {success}}
         `,
-        };
-        try {
-            const response = await axios.post("/graphql", graphQLquery);
-            console.log(response.data);
-        } catch (err) {
-            console.log(err);
-        }
-    };
-    return (
-        <PageWrapper>
-            <Typography variant="h1" align="center">
-                Zresetuj hasło
-            </Typography>
-            <form
-                onSubmit={handleSubmit(onSubmit)}
-                style={{
-                    display: "flex",
-                    flexDirection: "column",
-                    maxWidth: "400px",
-                    margin: "2rem auto",
-                }}
-            >
-                <Typography>
-                    Jeśli zapomniałeś hasła możesz je zresetować, aby odzyskać
-                    dostęp do swojego konta. Link otrzymasz na adres email
-                    podany podczas rejestracji
-                </Typography>
-                <TextField
-                    label="Twój adres email"
-                    error={errors.hasOwnProperty("email")}
-                    helperText={errors.email && "Podaj prawidłowy adres email"}
-                    margin="normal"
-                    inputRef={register({
-                        required: true,
-                        pattern: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
-                    })}
-                    name="email"
-                />
-                <Button variant="contained" color="primary" type="submit">
-                    Zresetuj hasło
-                </Button>
-            </form>
-        </PageWrapper>
-    );
+		};
+		try {
+			const response = await axios.post('/graphql', graphQLquery);
+			console.log(response);
+			if (response.data.data.sendResetPasswordEmail.success) {
+				history.push('/wyslano-link');
+			}
+		} catch (err) {
+			console.log(err);
+		}
+	};
+	return (
+		<PageWrapper>
+			<Typography variant="h1" align="center">
+				Zresetuj hasło
+			</Typography>
+			<form
+				onSubmit={handleSubmit(onSubmit)}
+				style={{
+					display: 'flex',
+					flexDirection: 'column',
+					maxWidth: '400px',
+					margin: '2rem auto',
+				}}
+			>
+				<Typography>
+					Jeśli zapomniałeś hasła możesz je zresetować, aby odzyskać
+					dostęp do swojego konta. Link otrzymasz na adres email
+					podany podczas rejestracji
+				</Typography>
+				<TextField
+					label="Twój adres email"
+					error={errors.hasOwnProperty('email')}
+					helperText={errors.email && 'Podaj prawidłowy adres email'}
+					margin="normal"
+					inputRef={register({
+						required: true,
+						pattern: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+					})}
+					name="email"
+				/>
+				<Button variant="contained" color="primary" type="submit">
+					Zresetuj hasło
+				</Button>
+			</form>
+		</PageWrapper>
+	);
 };
