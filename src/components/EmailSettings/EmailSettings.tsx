dc8342129ea415027f0ce409e5ee2b865e4ff473
Bruinen <max.machnik@gmail.com>
2020-05-07 13:45:15 +0200

    Email settings 50% done

--- src/components/EmailSettings/EmailSettings.tsx
+++ src/components/EmailSettings/EmailSettings.tsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
 import { useSelector } from "react-redux";
+import { useForm } from "react-hook-form";
 
 // MUI
 import {
@@ -7,7 +9,16 @@ import {
     CardContent,
     CardActions,
     Button,
+    FormControlLabel,
     Switch,
+    TextField,
+    Box,
+    FormControl,
+    Input,
+    InputLabel,
+    InputAdornment,
+    IconButton,
+    FormHelperText,
 } from "@material-ui/core";
 
 // Components
@@ -15,41 +26,133 @@ import { CenteredCard } from "../CenteredCard/CenteredCard";
 
 // Types
 import { State } from "../../types/State";
+import { Edit, Cancel } from "@material-ui/icons";
 
 interface EmailsState {
     emailInEdit: boolean;
     emailChanged: boolean;
-    newEmail?: string;
-    password?: string;
-    unsubscribed: boolean;
 }
 
-export const EmailSettings: React.FC = () => {
-    const user = useSelector((state: State) => state.auth);
+type ChangeResponse =
+    | "invalid-password"
+    | "success"
+    | "server-error"
+    | undefined;
 
+export const EmailSettings: React.FC = () => {
+    // Set unsubcribed from redux as default, but manage it in local state of useForm hook
+    const user = useSelector((state: State) => state.auth);
+    const { register, handleSubmit, errors, setValue, getValues } = useForm();
+    const [changeResponse, setChangeResponse] = useState<ChangeResponse>();
     const [emailsForm, setEmailsForm] = useState<EmailsState>({
         emailInEdit: false,
         emailChanged: false,
-        unsubscribed: false,
     });
 
+    const onSubmit = (data: any) => console.log(data);
+
+    const handleStartEditingEmail = () => {
+        setEmailsForm({ emailInEdit: true, emailChanged: false });
+    };
+
+    const handleCancelEditingEmail = () => {
+        setEmailsForm({ emailInEdit: false, emailChanged: false });
+        setValue("email", user.email);
+    };
+
     useEffect(() => {
-        setEmailsForm((prev) => ({
-            ...prev,
-            unsubscribed: user.unsubscribed!,
-        }));
-    }, []);
+        setValue("email", user.email);
+        setValue("unsubcribed", user.unsubscribed);
+    }, [user.email]);
+
     return (
         <CenteredCard>
-            <CardContent>
-                <Typography variant="h3">Powiadomienia email</Typography>
-                <form></form>
-            </CardContent>
-            <CardActions>
-                <Button variant="contained" color="primary" type="submit">
-                    Zapisz zmiany
-                </Button>
-            </CardActions>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <CardContent>
+                    <Typography variant="h3">Powiadomienia email</Typography>
+                    <Box
+                        padding={0.5}
+                        display="flex"
+                        flexDirection="column"
+                        maxWidth="400px"
+                    >
+                        <FormControl
+                            margin="normal"
+                            disabled={!emailsForm.emailInEdit}
+                        >
+                            <InputLabel htmlFor="email">
+                                Twój adres email
+                            </InputLabel>
+                            <Input
+                                id="email"
+                                error={errors.hasOwnProperty("email")}
+                                inputRef={register()}
+                                name="email"
+                                type="email"
+                                autoComplete="off"
+                                endAdornment={
+                                    <InputAdornment position="end">
+                                        <IconButton
+                                            aria-label="Edytuj adres email"
+                                            onClick={
+                                                emailsForm.emailInEdit
+                                                    ? handleCancelEditingEmail
+                                                    : handleStartEditingEmail
+                                            }
+                                        >
+                                            {emailsForm.emailInEdit ? (
+                                                <Cancel color="secondary" />
+                                            ) : (
+                                                <Edit />
+                                            )}
+                                        </IconButton>
+                                    </InputAdornment>
+                                }
+                            />
+                            {errors.email && (
+                                <FormHelperText>
+                                    Podaj prawidłowy adres email
+                                </FormHelperText>
+                            )}
+                        </FormControl>
+
+                        <TextField
+                            error={changeResponse === "invalid-password"}
+                            helperText={
+                                changeResponse === "invalid-password"
+                                    ? "Podane hasło jest nieprawidłowe"
+                                    : "Ze względów bezpieczeństwa podaj swoje hasło"
+                            }
+                            label="Hasło"
+                            margin="normal"
+                            inputRef={register({
+                                required: emailsForm.emailChanged,
+                                minLength: 5,
+                            })}
+                            name="password"
+                            type="password"
+                            autoComplete="off"
+                        />
+                        <FormControlLabel
+                            control={
+                                <Switch
+                                    checked={getValues().unsubcribed}
+                                    name="unsubscribed"
+                                    color="secondary"
+                                    inputRef={register()}
+                                />
+                            }
+                            label="Nie chcę otrzymywać powiadomień na email"
+                        />
+                    </Box>
+                </CardContent>
+
+                <CardActions>
+                    <Button variant="contained" color="primary" type="submit">
+                        Zapisz zmiany
+                    </Button>
+                </CardActions>
+            </form>
         </CenteredCard>
     );
 };
--- src/components/PasswordSettings/PasswordSettings.tsx
+++ src/components/PasswordSettings/PasswordSettings.tsx
@@ -1,36 +1,36 @@
-import React, { useState } from 'react';
-import axios from 'axios';
-import { Link as RouterLink, useHistory } from 'react-router-dom';
-import { useForm } from 'react-hook-form';
+import React, { useState } from "react";
+import axios from "axios";
+import { Link as RouterLink, useHistory } from "react-router-dom";
+import { useForm } from "react-hook-form";
 
 // MUI
 import {
-	Typography,
-	CardContent,
-	CardActions,
-	Button,
-	TextField,
-	Box,
-} from '@material-ui/core';
+    Typography,
+    CardContent,
+    CardActions,
+    Button,
+    TextField,
+    Box,
+} from "@material-ui/core";
 
 // Components
-import { CenteredCard } from '../CenteredCard/CenteredCard';
+import { CenteredCard } from "../CenteredCard/CenteredCard";
 
 type ChangeResponse =
-	| 'invalid-old-password'
-	| 'success'
-	| 'server-error'
-	| undefined;
+    | "invalid-old-password"
+    | "success"
+    | "server-error"
+    | undefined;
 
 export const PasswordSettings: React.FC = () => {
-	const history = useHistory();
-	const [changeResponse, setChangeResponse] = useState<ChangeResponse>();
-	const { register, handleSubmit, errors, getValues } = useForm();
-	const onSubmit = async (formData: any) => {
-		console.log(formData);
-		const { oldPassword, newPassword } = formData;
-		const graphQLquery = {
-			query: `
+    const history = useHistory();
+    const [changeResponse, setChangeResponse] = useState<ChangeResponse>();
+    const { register, handleSubmit, errors, getValues } = useForm();
+    const onSubmit = async (formData: any) => {
+        console.log(formData);
+        const { oldPassword, newPassword } = formData;
+        const graphQLquery = {
+            query: `
 			mutation{changePassword(changePasswordInput: 
 				{
 					newPassword: "${newPassword}",
@@ -38,85 +38,90 @@ export const PasswordSettings: React.FC = () => {
 				}
 			) {success message}}
         `,
-		};
-		try {
-			const response = await axios.post('/graphql', graphQLquery);
-			console.log(response.data);
-			if (response.data.data.changePassword.success) {
-				history.push('/logowanie');
-				// Add prompt about successfully changed password
-			} else {
-				throw new Error(response.data.data);
-			}
-		} catch (err) {
-			console.log(err);
-		}
-	};
-	return (
-		<CenteredCard>
-			<form onSubmit={handleSubmit(onSubmit)}>
-				<CardContent>
-					<Typography variant="h3">Zmiana hasła</Typography>
-					<Box
-						padding={0.5}
-						display="flex"
-						flexDirection="column"
-						maxWidth="400px"
-					>
-						<TextField
-							error={changeResponse === 'invalid-old-password'}
-							label="Stare hasło"
-							margin="normal"
-							inputRef={register({
-								required: true,
-								minLength: 5,
-							})}
-							name="oldPassword"
-							type="password"
-							autoComplete="off"
-						/>
-						<TextField
-							label="Nowe hasło"
-							error={errors.hasOwnProperty('newPassword')}
-							helperText={
-								errors.password &&
-								'Hasło musi zawierać co najmniej 5 znaków'
-							}
-							margin="normal"
-							inputRef={register({
-								required: true,
-								minLength: 5,
-							})}
-							name="newPassword"
-							type="password"
-						/>
-						<TextField
-							label="Powtórz nowe hasło"
-							error={errors.hasOwnProperty('repassword')}
-							helperText={
-								errors.repassword &&
-								'Hasła muszą być identyczne'
-							}
-							margin="normal"
-							inputRef={register({
-								validate: repassword =>
-									repassword === getValues().newPassword,
-							})}
-							name="repassword"
-							type="password"
-						/>
-					</Box>
-				</CardContent>
+        };
+        try {
+            const response = await axios.post("/graphql", graphQLquery);
+            console.log(response.data);
+            if (response.data.data.changePassword.success) {
+                history.push("/logowanie");
+                // Add prompt about successfully changed password
+            } else {
+                setChangeResponse("invalid-old-password");
+            }
+        } catch (err) {
+            console.log(err);
+            setChangeResponse("server-error");
+        }
+    };
+    return (
+        <CenteredCard>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <CardContent>
+                    <Typography variant="h3">Zmiana hasła</Typography>
+                    <Box
+                        padding={0.5}
+                        display="flex"
+                        flexDirection="column"
+                        maxWidth="400px"
+                    >
+                        <TextField
+                            error={changeResponse === "invalid-old-password"}
+                            helperText={
+                                changeResponse === "invalid-old-password" &&
+                                "Podane hasło jest nieprawidłowe"
+                            }
+                            label="Stare hasło"
+                            margin="normal"
+                            inputRef={register({
+                                required: true,
+                                minLength: 5,
+                            })}
+                            name="oldPassword"
+                            type="password"
+                            autoComplete="off"
+                        />
+                        <TextField
+                            label="Nowe hasło"
+                            error={errors.hasOwnProperty("newPassword")}
+                            helperText={
+                                errors.password &&
+                                "Hasło musi zawierać co najmniej 5 znaków"
+                            }
+                            margin="normal"
+                            inputRef={register({
+                                required: true,
+                                minLength: 5,
+                            })}
+                            name="newPassword"
+                            type="password"
+                        />
+                        <TextField
+                            label="Powtórz nowe hasło"
+                            error={errors.hasOwnProperty("repassword")}
+                            helperText={
+                                errors.repassword &&
+                                "Hasła muszą być identyczne"
+                            }
+                            margin="normal"
+                            inputRef={register({
+                                validate: (repassword) =>
+                                    repassword === getValues().newPassword,
+                            })}
+                            name="repassword"
+                            type="password"
+                        />
+                    </Box>
+                </CardContent>
 
-				<CardActions>
-					<Button variant="contained" color="primary" type="submit">
-						Zapisz zmiany
-					</Button>
-					<Button component={RouterLink} to="/zresetuj-haslo">
-						Zapomniałem hasła
-					</Button>
-				</CardActions>
-			</form>
-		</CenteredCard>
-	);
+                <CardActions>
+                    <Button variant="contained" color="primary" type="submit">
+                        Zapisz zmiany
+                    </Button>
+                    <Button component={RouterLink} to="/zresetuj-haslo">
+                        Zapomniałem hasła
+                    </Button>
+                </CardActions>
+            </form>
+        </CenteredCard>
+    );
 };
